0992d0658919585908b486f32d27c694bac04265
JokNavi <JokNavi1@gmail.com>
2023-11-06 13:40:35 +0100

    Impl fizz_buzz fn

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,19 @@ use std::collections::HashMap;
 pub const START: usize = 0;
 
 pub fn fizz_buzz(sets: &HashMap<usize, impl ToString>, stop: usize) -> String {
-    todo!();
+    (START..=stop)
+        .into_iter()
+        .map(|num| match sets.get(&num) {
+            Some(value) => value.to_string(),
+            None => num.to_string(),
+        })
+        .collect::<Vec<String>>()
+        .join("\n")
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
 
     #[test]
     fn test_fizz_buzz() {
