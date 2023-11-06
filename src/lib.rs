4f1eb2acc65df92085c43c723ca94957b3db1a12
JokNavi <JokNavi1@gmail.com>
2023-11-06 12:52:34 +0100

    Impl fizz_buzz frame

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+pub const START: usize = 0;
+
+pub fn fizz_buzz<A, B>(sets: &HashMap<usize, String>, stop: usize) {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    
+}
