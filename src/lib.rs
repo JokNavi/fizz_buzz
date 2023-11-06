b5060e4311f6d633966159acc1fedbb45f1705db
JokNavi <JokNavi1@gmail.com>
2023-11-06 13:24:19 +0100

    Impl fizz_buzz test and rewrite fizz_buzz fn

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 pub const START: usize = 0;
 
-pub fn fizz_buzz<A, B>(sets: &HashMap<usize, String>, stop: usize) {
+pub fn fizz_buzz(sets: &HashMap<usize, impl ToString>, stop: usize) -> String {
     todo!();
 }
 
@@ -10,5 +10,10 @@ pub fn fizz_buzz<A, B>(sets: &HashMap<usize, String>, stop: usize) {
 mod tests {
     use super::*;
 
-    
+
+    #[test]
+    fn test_fizz_buzz() {
+        let sets = HashMap::from([(3, "fizz"), (5, "buzz")]);
+        assert_eq!(fizz_buzz(&sets, 5), "0\n1\n2\nfizz\n4\nbuzz");
+    }
 }
