747b4d1fa60cc1f72c561310e64d4d321999b703
WannesVantorre <wannes.vantorre@student.howest.be>
2023-11-08 12:57:28 +0100

    Remake fizz_buzz fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
-use std::collections::HashMap;
-
 pub const START: usize = 0;
 
-pub fn fizz_buzz(sets: &HashMap<usize, impl ToString>, stop: usize) -> String {
+pub fn fizz_buzz(sets: &[(usize, impl ToString)], stop: usize) -> Vec<String> {
     (START..=stop)
-        .map(|num| match sets.get(&num) {
-            Some(value) => value.to_string(),
-            None => num.to_string(),
+        .map(|num| -> String {
+            let total_word = sets.iter()
+                .filter(|(set_num, _)| num % set_num == 0)
+                .map(|(_, word)| word.to_string())
+                .collect::<Vec<String>>()
+                .join("");
+            if total_word.len() == 0 {
+                return num.to_string();
+            }
+            total_word
         })
         .collect::<Vec<String>>()
-        .join("\n")
 }
 
 #[cfg(test)]
@@ -18,7 +22,16 @@ mod tests {
 
     #[test]
     fn test_fizz_buzz() {
-        let sets = HashMap::from([(3, "fizz"), (5, "buzz")]);
-        assert_eq!(fizz_buzz(&sets, 5), "0\n1\n2\nfizz\n4\nbuzz");
+        let sets = [(3usize, "fizz"), (5, "buzz")];
+        assert_eq!(
+            &fizz_buzz(&sets, 5),
+            &["fizzbuzz", "1", "2", "fizz", "4", "buzz"]
+        );
+    }
+
+    #[test]
+    fn example() {
+        let sets = [(3, "fizz"), (5, "buzz")];
+        println!("{}", fizz_buzz(&sets, 50).join("\n"));
     }
 }
